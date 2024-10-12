32fd96be0f0b0ade41054c69496a7dd61e64cfc3
alex <dapine@gmail.com>
2024-10-12 11:27:11 -0500

    Initial commit

---
+++ src/main/java/servicio/sofkaclientepersona/SofkaClientePersonaApplication.java
@@ -0,0 +1,13 @@
+package servicio.sofkaclientepersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaClientePersonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofkaClientePersonaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/servicio/sofkaclientepersona/SofkaClientePersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package servicio.sofkaclientepersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkaClientePersonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
